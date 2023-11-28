5688642264911e43a2bedc9da0e8afa159c7fe25
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-28 21:10:27 +0300

    add logs and environment variable for server port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  Logger.log(`Server start on port: ${port}`);
+  Logger.log(`${JSON.stringify(configService.get('POSTGRES_USER'))}`);
+  await app.listen(port);
 }
 bootstrap();
