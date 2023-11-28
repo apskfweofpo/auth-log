036f8319c8a0c0a33e3f8843f979ac201641eb90
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-28 21:11:58 +0300

    creater file for configuration server and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,21 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: +configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [],
-        synchronize: true,
+        ...configService.get('database'),
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     ConfigModule.forRoot({
-      envFilePath: '.env'
+      load: [configuration],
     }),
-    UsersModule],
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/configuration.ts
@@ -0,0 +1,15 @@
+import { User } from "src/users/entities/user.entity";
+
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 5000,
+    database: {
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+      username:  process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: true,
+    }
+  });
