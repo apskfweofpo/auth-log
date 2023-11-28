1ab8e08b90453c500076b6f5769d8a74b8539ef9
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-29 01:57:07 +0300

    add enpoint for change password for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ChangePasswordDto } from './dtos/change-password.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('change-password')
+    changePassword(@Body() changePasswordDto: ChangePasswordDto) {
+        this.usersService.changePassword(changePasswordDto);
+    }
+}
