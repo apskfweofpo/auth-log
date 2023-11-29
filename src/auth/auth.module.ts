10555f665f2dd6ccb1cbe71702316448d1aacaf2
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-30 02:35:05 +0300

    add configmodule for jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,26 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import jwtConfig from 'src/config/jwt.config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     forwardRef(() => UsersModule),
-    JwtModule.register({
-      secret: 'test',
-      signOptions: {
-        expiresIn: '24h',
-      },
+    ConfigModule.forRoot({
+      load: [jwtConfig],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('secret'),
+        signOptions: {
+          expiresIn: configService.get<string>('expiresIn'),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   exports: [AuthService, JwtModule],
