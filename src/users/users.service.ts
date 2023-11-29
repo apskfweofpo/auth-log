a88999a50d2976f315c9b18dadb6250cb093f625
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-29 23:08:04 +0300

    add methods: createUser, getUserByEmail, getUserByUsername, updatePassword, findById

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new User();
+    newUser.email = createUserDto.email;
+    newUser.username = createUserDto.username;
+    newUser.password = createUserDto.password;
+
+    return await this.userRepository.save(newUser);
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    return user;
+  }
+
+  async getUserByUsername(username: string) {
+    const user = await this.userRepository.findOne({ where: { username } });
+    return user;
+  }
+
+  async updatePassword(user: User): Promise<User> {
+    const existingUser = await this.findById(user.id);
+
+    if (!existingUser) {
+      throw new NotFoundException('Пользователь не найден');
+    }
+
+    existingUser.password = user.password;
+    return this.userRepository.save(existingUser);
+  }
+
+  async findById(id: number): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
