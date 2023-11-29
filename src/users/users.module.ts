9749c1fb4c0ab568b2b9a7f06d4da63175c4514c
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-30 02:34:04 +0300

    add import typeorm

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  providers: [UsersService],
   controllers: [UsersController],
-  providers: [UsersService]
+  exports: [UsersService],
 })
 export class UsersModule {}
