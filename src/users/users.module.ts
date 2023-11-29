f1a99d30e4895e2c1632cd7bdea8459cdda48639
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-30 02:37:19 +0300

    remove controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,4 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
@@ -8,7 +7,6 @@ import { AuthModule } from 'src/auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   providers: [UsersService],
-  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UsersModule {}
