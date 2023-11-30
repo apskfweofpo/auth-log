c80bb8b2e40b83432ad89c48bd41a31940e070e7
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-30 04:14:31 +0300

    add logs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,20 +9,22 @@ import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/users/entities/user.entity';
-import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { ResetPasswordDto } from './dtos/ResetPassword.dto';
 import { LoginDto } from './dtos/Login.dto';
 import { RegistrationDto } from './dtos/Registration.dto';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private readonly logger: MyLoggerService,
   ) {}
 
   async login(userDto: LoginDto) {
     const user = await this.validateUser(userDto);
+    this.logger.warn(`The user logged in. Id:${user.id}`);
     return this.generateToken(user);
   }
 
@@ -32,6 +34,9 @@ export class AuthService {
     );
 
     if (existingByEmail) {
+      this.logger.error(
+        `This email is already registered. Email:${existingByEmail}`,
+      );
       throw new HttpException(
         'This email is already registered',
         HttpStatus.BAD_REQUEST,
@@ -43,6 +48,9 @@ export class AuthService {
     );
 
     if (existingByUserName) {
+      this.logger.error(
+        `This username is already registered. Username:${existingByUserName}`,
+      );
       throw new HttpException(
         'This username is already registered',
         HttpStatus.BAD_REQUEST,
@@ -54,8 +62,10 @@ export class AuthService {
       ...userDto,
       password: hashPassword,
     });
+    this.logger.warn(`The user has registered. Id:${user.id}`);
     return this.generateToken(user);
   }
+
   private generateToken(user: User) {
     const payload = { id: user.id };
     return {
@@ -66,6 +76,7 @@ export class AuthService {
   private async validateUser(userDto: LoginDto) {
     const user = await this.usersService.getUserByEmail(userDto.email);
     if (!user) {
+      this.logger.error(`User not found by Email. Email:${user.email}`);
       throw new UnauthorizedException({ message: 'Invalid Email' });
     }
     const passwordEquals = await bcrypt.compare(
@@ -73,20 +84,23 @@ export class AuthService {
       user.password,
     );
     if (!passwordEquals) {
+      this.logger.error(`Invalid password Id:${user.id}`);
       throw new UnauthorizedException({ message: 'Invalid password' });
     }
+    this.logger.warn(`The user has validated. Id:${user.id}`);
     return user;
   }
 
   async resetPassword(userId: number, resetPasswordDto: ResetPasswordDto) {
     const user = await this.usersService.findById(userId);
     if (!user) {
-      throw new NotFoundException('Пользователь не найден');
+      this.logger.error(`The User not find. Id:${user.id}`);
+      throw new NotFoundException('User not find');
     }
 
     const hashPassword = await bcrypt.hash(resetPasswordDto.password, 3);
     user.password = hashPassword;
-
+    this.logger.warn(`The user reset password. Id:${user.id}`);
     await this.usersService.updatePassword(user);
   }
 }
