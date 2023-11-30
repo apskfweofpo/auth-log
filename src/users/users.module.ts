af64afa56c74b051647a7cae28a13dce0c571a2e
Pavel Krikunov <fusrodah.df@mail.ru>
2023-11-30 04:13:53 +0300

    add typeorm module and logger

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,14 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { MyLoggerModule } from 'src/my-logger/my-logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule),
+    MyLoggerModule,
+  ],
   providers: [UsersService],
   exports: [UsersService],
 })
